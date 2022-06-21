905c3baf0a90484661634e1ea3bbb7b494a00654
Ali <h3rzius@gmail.com>
2022-06-21 20:07:24 +0200

    Version 1.0.0.1 First Commit

---
+++ src/main/java/me/h3rzius/portableshop/PortableShop.java
@@ -0,0 +1,17 @@
+package me.h3rzius.portableshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PortableShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
