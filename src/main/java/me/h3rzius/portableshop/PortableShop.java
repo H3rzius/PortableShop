7ce029eaf61f53cbcebc3768ad10063c1bb8cad3
H3rzius <h3rzius@gmail.com>
2022-08-17 22:26:24 +0200

    1.0.2.2 Adding a database to localhost for testing

--- src/main/java/me/h3rzius/portableshop/PortableShop.java
+++ src/main/java/me/h3rzius/portableshop/PortableShop.java
@@ -4,6 +4,11 @@ import me.h3rzius.portableshop.commands.MainCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public final class PortableShop extends JavaPlugin {
     Player player;
 
@@ -11,6 +16,23 @@ public final class PortableShop extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getCommand("portableshop").setExecutor(new MainCommand());
+
+        String dbUrl = "jdbc:mysql://localhost/pshop";
+        String dbName = "root";
+        String dbPassword = "";
+
+        try {
+            Connection connection = DriverManager.getConnection(dbUrl, dbName, dbPassword);
+            System.out.println("Database connected");
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS player_info(uuid varchar(36) primary_key, has_shop bool, pages int)";
+            statement.execute(sql);
+
+        } catch (SQLException e) {
+            System.out.println("Cannot connect to database:");
+            e.printStackTrace();
+        }
 
     }
 
